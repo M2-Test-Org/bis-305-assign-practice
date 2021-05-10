4b372cf21878042b7cf739ececfc1fc4abd844fc
m2web <m2web@yahoo.com>
2021-05-10 15:35:51 -0400

    Add project files.

---
+++ Lyrics.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+namespace bis_305_assign_practice
+{
+    class Lyrics
+    {
+        public static void Main()
+        {
+            WriteLine("Roses are red,");
+            WriteLine("Violets are blue,");
+            WriteLine("BIS 305 is awesome,");
+            WriteLine("And so are you!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# BIS 305 Assignment Practice 1-5.
+
+This repo is for use to code and debug the BIS 305 Assignment Practice - Programming Exercise 1-5.
+
+The output from the console should be four lyric lines of text like:
+```html
+Roses are red
+Violets are blue
+BIS 305 is awesome
+And so are you!
+```
